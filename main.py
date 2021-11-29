300b1371dcaad6b8f59693e4d5c889fe2136430c
arslan-cyberTITU <rasulov.titu@gmail.com>
2021-11-29 17:34:32 +0500

    1

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ app.add_middleware(
 
 Base.metadata.create_all(engine)
 
-app.include_router(students_router,       tags=["Students"])
-app.include_router(parents_router,        tags=["Parents"])
-app.include_router(student_detail_router, tags=["Student Detail"])
-app.include_router(faculty_router,        tags=["Faculty"])
-app.include_router(welayat_router,        tags=["Welayat"])
-app.include_router(parent_status_router,  tags=["Parent Status"])
+app.include_router(students_router        , tags=["Students"])
+app.include_router(parents_router         , tags=["Parents"])
+app.include_router(student_detail_router  , tags=["Student Detail"])
+app.include_router(faculty_router         , tags=["Faculty"])
+app.include_router(welayat_router         , tags=["Welayat"])
+app.include_router(parent_status_router   , tags=["Parent Status"])
