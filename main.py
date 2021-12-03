cf78336603a1bf1a4a7f8cad414dae45da6d3eca
arslan-cyberTITU <rasulov.titu@gmail.com>
2021-12-03 10:28:54 +0500

    second

--- main.py
+++ main.py
@@ -28,6 +28,3 @@ app.include_router(student_detail_router  , tags=["Student Detail"])
 app.include_router(faculty_router         , tags=["Faculty"])
 app.include_router(welayat_router         , tags=["Welayat"])
 app.include_router(parent_status_router   , tags=["Parent Status"])
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True, debug=False)
