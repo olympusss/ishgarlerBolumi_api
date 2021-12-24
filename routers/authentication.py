2123f766b24fa0c47f9dbef046716738c79aeb46
arslan-cyberTITU <rasulov.titu@gmail.com>
2021-12-24 10:06:14 +0500

    commit

--- routers/authentication.py
+++ routers/authentication.py
@@ -10,6 +10,16 @@ authentication_router = APIRouter()
 
 @authentication_router.post("/sign-up")
 def log_on(req: sign_up, db: Session = Depends(get_db)):
+    get_user = db.query(
+        Registration.username,
+        Registration.password
+    ).filter(
+        and_(
+            Registration.username == req.username, 
+            Registration.password == req.password
+        )).first()
+    if get_user:
+        return Returns.BODY_NULL
     token_dict = {
         "username" : req.username,
         "password" : req.password
@@ -32,18 +42,13 @@ def log_on(req: sign_up, db: Session = Depends(get_db)):
         return Returns.NOT_INSERTED
     
 @authentication_router.post("/sign-in")
-def sign_in(req: sign_in, header_param: Request, db: Session = Depends(get_db)):
-    get_token = check_token(header_param=header_param)
-    if not get_token:
-        return Returns.TOKEN_NOT_FOUND
-    get_user = db.query(Registration.id).\
+def sign_in(req: sign_in, db: Session = Depends(get_db)):
+    get_user = db.query(Registration.token).\
         filter(and_(
             Registration.username == req.username,
             Registration.password == req.password,
-            Registration.token    == get_token
-        )).all()
+        )).first()
     if not get_user:
         return Returns.BODY_NULL
     else:
-        token_res = {"token" : get_token}
-        return Returns.object(token_res)
+        return Returns.object(get_user)
