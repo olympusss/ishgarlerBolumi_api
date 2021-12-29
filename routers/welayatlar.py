4f8a2b3ec465efb6741525741acca64f131cb3d2
arslan-cyberTITU <rasulov.titu@gmail.com>
2021-12-29 09:32:29 +0500

    commit

--- routers/welayatlar.py
+++ routers/welayatlar.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from db import get_db
 from models import Welayatlar
 from returns import Returns
+import json
 
 welayat_router = APIRouter()
 
@@ -12,7 +13,24 @@ def get_welayat(db: Session = Depends(get_db)):
         Welayatlar.id,
         Welayatlar.name,
     ).all()
-    if not result:
-        return Returns.BODY_NULL
+    
+    if result:
+        return Returns.object(result)
+    f = open('json/welayatlar.json')
+    data = json.load(f)
+    for i in data:
+        name_json: str = i.get('name')
+        new_add = Welayatlar(name = name_json)
+        db.add(new_add)
+        db.commit()
+        db.refresh(new_add)
+        if not new_add:
+            return Returns.NOT_INSERTED
+    result = db.query(
+        Welayatlar.id,
+        Welayatlar.name,
+    ).all()
+    if result:
+        return Returns.object(result)
     else:
-        return result
+        return Returns.BODY_NULL
